906b7877d384c57927cdf5af40d14427825d04bf
Justinien Ghorra <justinien.ghorra8@etu.univ-lorraine.fr>
2016-12-11 15:06:50 +0100

    doc pour bus

--- bus.cpp
+++ bus.cpp
@@ -6,6 +6,19 @@ Bus::Bus() : Vehicule() {
 
 }
 
+/**
+ * @brief Constructeur de Bus
+ * @param km le kilomètrage du bus
+ * @param cap sa capacité maximale
+ * @param height sa hauteur
+ * @param weight son poids
+ * @param mod son modèle (exemple : "CITARO")
+ * @param marq sa marque (exemple : "Mercedes")
+ * @param color sa couleur
+ * @param immat le numéro de sa plaque d'immatriculation
+ * @param an l'année de son achat
+ * @param price son prix (par jour)
+ */
 Bus::Bus(int km, int cap, double height, double weight, std::string mod, std::string marq, std::string color, std::string immat, int an, int price) :
     Vehicule(mod, marq, color, immat, an, price){
     kilometrage = km;
@@ -15,22 +28,45 @@ Bus::Bus(int km, int cap, double height, double weight, std::string mod, std::st
     prixOption = 96;
 }
 
+/**
+ * @brief getter du kilométrage
+ * @return le kilométrage du bus
+ */
 int Bus::getKilo() {
     return kilometrage;
 }
 
+/**
+ * @brief getter de la capacité maximale
+ * @return la capacité maximale du bus
+ */
 int Bus::getCapacite() {
     return capacite;
 }
 
+/**
+ * @brief getter de la hauteur
+ * @return la hauteur du bus
+ */
 double Bus::getHauteur() {
     return hauteur;
 }
 
+/**
+ * @brief getter du poids
+ * @return le poids du bus
+ */
 double Bus::getPoids() {
     return poids;
 }
 
+/**
+ * @brief Construit la fiche détaillée du bus
+ * @return la fiche contenue dans un QString
+ * Construit la fiche détaillée du bus pour l'afficher
+ * lors de la validation du véhicule par le loueur.
+ * Stocké dans un QString pour remplir le QLabel associé
+ */
 QString Bus::toQStringDetail() {
     return QString((this->getMarque()+"\n"
                     +this->getModele()+"\n"
@@ -42,6 +78,7 @@ QString Bus::toQStringDetail() {
                     +patch::to_string(this->getHauteur())+"\n"
                     +patch::to_string(this->getPoids())).c_str());
 }
+
 
 QString Bus::attributsFicheDetail() {
     return QString ("Marque\nModèle\nCouleur\nPrix par jour (€)\nAnnée\nKilométrage\nCapacité\nHauteur (m)\nPoids (t)");
